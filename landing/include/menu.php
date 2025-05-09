4e6a5511a62c517cdb35c961003f6f4db6f8df81
shareiar islam <shareiarshams30015@gmail.com>
2025-05-10 04:40:34 +0600

    change menu bar and add active class with current route

--- landing/include/menu.php
+++ landing/include/menu.php
@@ -1,3 +1,7 @@
+<?php
+    $currentPage = basename($_SERVER['PHP_SELF']);
+?>
+
 <!-- Navbar Start -->
 <div class="container-fluid p-0">
     <nav class="navbar navbar-expand-lg bg-dark navbar-dark py-2 py-lg-0 px-lg-5">
@@ -9,18 +13,11 @@
         </button>
         <div class="collapse navbar-collapse justify-content-between px-0 px-lg-3" id="navbarCollapse">
             <div class="navbar-nav mr-auto py-0">
-                <a href="index.html" class="nav-item nav-link active">Home</a>
-                <a href="category.html" class="nav-item nav-link">Category</a>
-                <a href="single.html" class="nav-item nav-link">Single News</a>
-                <div class="nav-item dropdown">
-                    <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">Dropdown</a>
-                    <div class="dropdown-menu rounded-0 m-0">
-                        <a href="#" class="dropdown-item">Menu item 1</a>
-                        <a href="#" class="dropdown-item">Menu item 2</a>
-                        <a href="#" class="dropdown-item">Menu item 3</a>
-                    </div>
-                </div>
-                <a href="contact.html" class="nav-item nav-link">Contact</a>
+                <a href="index.php" class="nav-item nav-link <?= ($currentPage == 'index.php') ? 'active' : '' ?>">Home</a>
+                <a href="category.php" class="nav-item nav-link <?= ($currentPage == 'category.php') ? 'active' : '' ?>">Category</a>
+                
+                 
+                <a href="contact.php" class="nav-item nav-link <?= ($currentPage == 'contact.php') ? 'active' : '' ?>">Contact</a>
             </div>
             <div class="input-group ml-auto d-none d-lg-flex" style="width: 100%; max-width: 300px;">
                 <input type="text" class="form-control border-0" placeholder="Keyword">
