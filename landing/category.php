1cf9c25f56e5bb162e02e5c460fddf91aa844253
shareiar islam <shareiarshams30015@gmail.com>
2025-05-10 04:42:25 +0600

    add cateogry and cateogry components page

---
+++ landing/category.php
@@ -0,0 +1,14 @@
+<?php 
+
+	function main_section(){
+
+		include '../components/landingCategory.php';
+		?>
+		<div class="mt-5">
+			
+		</div>
+		<?php
+		include 'include/sidebar.php';
+	} 
+	include('layout.php');
+?>
